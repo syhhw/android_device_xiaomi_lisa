bfd3c2d023881c4786b1fd8d99d26a69ed042004
ganeshi4u <ganeshvarmai4u@gmail.com>
2021-10-20 16:42:27 +0000

    lisa: Force compilation using prebuilt kernel
    - We do not yet have the source code so use headers from 8350

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,5 +13,15 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := lisa
 
+# Kernel
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_PREBUILT_KERNEL := prebuilt/kernel/Image
+TARGET_PREBUILT_DTB := prebuilt/kernel/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := prebuilt/kernel/dtbo.img
+
+TARGET_KERNEL_CONFIG := vendor/lahaina_GKI
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
+
 # Include proprietary files
 include vendor/xiaomi/lisa/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from sm8350-common
 $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
+# Kernel
+PRODUCT_COPY_FILES += \
+    prebuilt/kernel/dtb.img:dtb.img
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
