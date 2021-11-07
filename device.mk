73775a7b83ff4775474a8c13989eb18f09df8d0c
Arian <arian.kulmer@web.de>
2021-11-07 19:21:49 +0100

    lisa: overlay: Configure display cutout and rounded corners
    Change-Id: I4773f8c2a4678b70ffea05831c7d4843c534f818

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 # Kernel
 PRODUCT_COPY_FILES += \
     prebuilt/kernel/dtb.img:dtb.img
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
